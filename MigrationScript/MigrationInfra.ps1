ed74bb841eb90e7a722fd90fbda894ceddf3be2f
Suhani Shrivastava <sshrivastava@microsoft.com>
2022-02-18 14:06:57 +0530

    Update MigrationInfra.ps1

--- MigrationScript/MigrationInfra.ps1
+++ MigrationScript/MigrationInfra.ps1
@@ -6,7 +6,7 @@
 [string]$tenantId = "72f988bf-86f1-41af-91ab-2d7cd011db47",
 
 #[Parameter(Mandatory=$true)]
-[string]$providerType = "SapHana",
+[string]$providerType = $null,
 
 #[Parameter(Mandatory=$true)]
 [string]$amsv1ArmId = "/subscriptions/53990dba-8128-4100-bb6d-ed38861c9f8f/resourceGroups/sakhare_ams_hana/providers/Microsoft.HanaOnAzure/sapMonitors/sakhare_ams4"
@@ -24,6 +24,7 @@
 . $PSScriptRoot\ListSapHanaProviders.ps1
 . $PSScriptRoot\ListSapNetWeaverProviders.ps1
 . $PSScriptRoot\GetSecretValue.ps1
+. $PSScriptRoot\ProviderTypePrompt.ps1
 # #############################
 
 function Get-Token($typeToken)
@@ -38,6 +39,16 @@ function Main
 {
     $global:ENABLE_TRACE = $true
     $logger = New-Object ConsoleLogger
+
+    $logFilePath = Join-Path $PSScriptRoot '\MigrationLog.txt'
+    Start-Transcript -Path $logFilePath
+
+    $logger.LogInfo("-----------Starting migration to AMSv2--------------")
+
+    if($providerType -like $null)
+    {
+        $providerType = Check-ProviderTypeInput
+    }
 
     #Uncomment the line below if running on local PowerShell
     #Connect-AzAccount
@@ -71,35 +82,37 @@ function Main
 
         $secret = Get-SecretValue $i.id
 
-        if ($secret.type -like "saphana" -and $providerType -like "saphana")
+        if ($secret.type -like "saphana" -and $providerType -like "saphana" -or $providerType -like "all")
         {
                if(!$secret.properties.hanaDbPasswordKeyVaultUrl)
                {
-                   $settings = $secret.properties
+                    $settings = $secret.properties
 
-                   #TODO: Transform provider setting
-                   $newSettings
+                    #TODO: Transform provider setting
+                    $newSettings
 
-                   $request = @{
+                    $request = @{
                         name = $secret.name
                         type = $secret.type
-                   }
+                    }
 
-                   $saphanaTransformedList.Add($request) | Out-Null
-                   $logger.LogInfoObject("Adding the following transformed SapHana object to migration list", $request)
+                    $saphanaTransformedList.Add($request) | Out-Null
+                    $logger.LogInfoObject("Adding the following transformed SapHana object to migration list", $request)
+                    
                }
                else
                {
-                   $request = @{
+                    $request = @{
                         name = $secret.name
                         type = $secret.type
-                   }
-                   $unsupportedProviderList.Add($request)
-                   $logger.LogError("Unsupported Type - SapHana Integrated KeyVault","100", "Please wait for the support to be enabled in AMSv2 to migrate provider - " + $secret.name)
+                    }
+                    $unsupportedProviderList.Add($request)
+                    $logger.LogError("Unsupported Type - SapHana Integrated KeyVault","100", "Please wait for the support to be enabled in AMSv2 to migrate provider - " + $secret.name)                  
                }
         }
-        elseif($secret.type -like "sapnetweaver" -and $providerType -like "sapnetweaver")
+        elseif($secret.type -like "sapnetweaver" -and $providerType -like "sapnetweaver" -or $providerType -like "all")
         {
+            #TODO: Sample snippet here. Enhance the code
             $request = @{
             name = $secret.name
             type = $secret.type
@@ -107,21 +120,35 @@ function Main
 
             $sapNetWeaverTransformedList.Add($request) | Out-Null
             $logger.LogInfoObject("Adding the following transformed SapNetWeaver object to migration list", $request)
+           
         }
         else
         {
-            $request = @{
-            name = $secret.name
-            type = $secret.type
+            if($secret.type -notlike "sapnetweaver" -and $secret.type -notlike "saphana")
+            {
+                $request = @{
+                name = $secret.name
+                type = $secret.type
+                }
+                
+                $unsupportedProviderList.Add($request) | Out-Null
             }
-
-            $unsupportedProviderList.Add($request) | Out-Null
         }
     }
+
+    Stop-Transcript
 
     Get-SapHanaProvidersList $saphanaTransformedList
     Get-SapNetWeaverProvidersList $sapNetWeaverTransformedList
     Get-UnsupportedProvidersList $unsupportedProviderList
+
+    Start-Transcript -Append -Path $logFilePath
+
+    $logger.LogInfoObject("Migrated AMSv1 SapHana List - ", $saphanaTransformedList)
+    $logger.LogInfoObject("Migrated AMSv1 SapNeWeaver List - ", $sapNetWeaverTransformedList)
+    $logger.LogInfoObject("Not Migrated AMSv1 Unsupported Provider list - ", $unsupportedProviderList)
+
+    Stop-Transcript
 }
 
 Main
